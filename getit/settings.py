2173233914b5ab751b26f77ce3710f0938a9e905
Ana Clara <anacmm2@al.insper.edu.br>
2025-09-15 08:05:40 -0300

    tentando publicar

--- getit/settings.py
+++ getit/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'getit.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='projeto-1-parte-b-anaclaramartinelli20.onrender.com/',
+        default='projeto-1-parte-b-anaclaramartinelli20.onrender.com',
         conn_max_age=600,
         ssl_require=not DEBUG
     )
